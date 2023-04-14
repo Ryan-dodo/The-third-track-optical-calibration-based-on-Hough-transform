da73d6d2b3a685eeca7d596d4dec3ad24fe9ab24
Ryan <93061945+Ryan-dodo@users.noreply.github.com>
2023-04-14 16:31:25 +0800

    增加绘制投票点

--- hofu.py
+++ hofu.py
@@ -1,13 +1,12 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
 import math
 
 # @Author  : Ryan
 # @Email   : ryan1057@csu.edu.cn
 # @File    : hofu.py
 # @Software: PyCharm
-# @Time    : 2023-04-14 09:29
+# @Time    : 2023-04-14 16:30
 # @Github  : https://github.com/Ryan-dodo/The-third-track-optical-calibration-based-on-Hough-transform
 # @using   : 绘制霍夫空间
 
@@ -38,7 +37,7 @@ for j in range(0, length_zu):
     rho = []
     for i in theta:
         rho.append(x[j]*math.cos(i)+y[j]*math.sin(i))
-    ax.plot(theta,rho,alpha = 1/30,c='g')
+    ax.plot(theta,rho,alpha = 1/100,c='g')
     del rho
 
 
@@ -55,13 +54,23 @@ while temp < 350:
     rho.append(temp)
     temp = temp + 1
 
+# 绘制网格
 for i in theta:
     ax.axvline(x=i, ymin=-50, ymax=350)
 for i in rho:
     ax.axhline(y=i, xmin=0, xmax=3.15)
 
+# 选出票数大于200的点
+max_val = [293, 287, 283, 258, 248, 244, 228, 220, 201]
+max_theta = [77, 79, 78, 76, 234, 235, 232, 233, 236]
+max_rho = [175, 178, 176, 173, 179, 178, 182, 181, 176]
 
-
+for i in range(len(max_val)):
+    max_rho[i] = max_rho[i] - 50
+    max_theta[i] = max_theta[i] / 100
+    max_val[i] = max_val[i] / 100 * 3
+for i in range(len(max_val)):
+    plt.scatter(max_theta[i], max_rho[i],s=max_val[i]**2,c='b')
 
 plt.show()  # 显示
 # plt.savefig('xx4.jpg')
